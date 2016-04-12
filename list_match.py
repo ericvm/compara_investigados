c71fa349742677e99da67383dfc044477fbb8b36
Eric Velten de Melo <eric.velten@eldorado.org.br>
2016-04-12 10:37:28 -0300

    Retificado erro na lista

--- list_match.py
+++ list_match.py
@@ -12,5 +12,8 @@ nomes_contra = load_list(contra)
 
 pro_investigados = set(nomes_pro) & set(nomes_investigados)
 contra_investigados = set(nomes_contra) & set(nomes_investigados)
+nenhum = set(nomes_investigados)
+nenhum = nenhum - set(nomes_pro) - set(nomes_contra)
+print(nenhum)
 print("A favor investigados:", len(pro_investigados), "de", len(nomes_pro))
 print("Contra investigados:", len(contra_investigados), "de", len(nomes_contra))
